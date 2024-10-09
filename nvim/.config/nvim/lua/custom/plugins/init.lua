6557eacf78e57b881fa1dd68d7d1cc3a8977c3f9
Kano-1 <jose.castrog@usm.cl>
2024-10-09 18:42:50 -0300

    Kitty, i3, hyprland and nvim added
    also a wallpaper!

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,14 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+
+  require 'custom/plugins/oil-vim',
+  require 'custom/plugins/noice',
+  require 'custom/plugins/dashboard-vim',
+  require 'custom/plugins/vim-go',
+  require 'custom/plugins/toggleterm',
+  require 'custom/plugins/harpoon',
+  require 'custom/plugins/undotree',
+}
---
+++ nvim/.config/nvim/lua/custom/plugins/vim-go.lua
@@ -0,0 +1,7 @@
+--
+--VIM-GO
+
+return {
+  'fatih/vim-go',
+  event = 'VeryLazy',
+}
